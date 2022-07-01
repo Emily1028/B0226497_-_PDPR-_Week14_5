bf35a2230cc05ea9b8566514cdb60bf09c223c8c
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 23:35:15 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0226497_段昱如_PDPR__Week14_5
+{
+    class Program
+    {
+        public static void CM(int a)
+        {
+            if (a > 150)
+                Console.Write("身高:" + a + " 全票");
+            else if(a <= 150&&a>=120)
+                Console.Write("身高:" + a + " 半票");
+            else 
+                Console.Write("身高:" + a + " 免費");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B0226497_段昱如_PDPR__Week14_5");
+            int cm;
+            Console.Write("請輸入身高值:");
+            cm = int.Parse(Console.ReadLine());
+            CM(cm);
+            Console.ReadLine();
+        }
+    }
+}
